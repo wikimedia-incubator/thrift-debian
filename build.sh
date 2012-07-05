e17e1b8316ada4081ad978502ded85e76c2be411
Andrew Otto <acotto@gmail.com>
2012-07-05 11:23:04 -0400

    build.sh - adding ability to build different thrift versions

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@
 # This repository exists for building thrift deb packages
 # for the Wikimedia Foundation.
 
-# TODO: 
-# - handle releases of thrift
 
 # Must be root to install packages
 if [ "$(id -u)" != "0" ]; then
@@ -12,34 +10,48 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-# Set thrift version to the version you'd like to build
-THRIFT_VERSION="0.8.0"
-repo_root="${PWD}"
+# package architecture (probably amd64)
+DEB_BUILD_ARCH=$(dpkg-architecture -qDEB_BUILD_ARCH | cut -d ';' -f 1)
+
+# package to build, either thrift, fb303, or all.
+packages=$1
+if [ -z $packages ]; then
+	packages="all"
+fi
+
+# The thrift version to the version to build.  Must be at thrift-$THRIFT_VERSION
+thrift_version=${2}
+: ${thrift_version:="0.2.0"} # default to 0.2.0
+
+# The repository root.
+repo_root="$(readlink -f $(dirname $0))"
+thrift_directory="${repo_root}/thrift-${thrift_version}"
+
 
 function build_thrift {
 	## Build thrift packages
 	echo "Building thrift packages..."
 
-	cd ${repo_root}/thrift-${THRIFT_VERSION}
+	cd $thrift_directory
 	dpkg-buildpackage
 
 	# This should build:
-	# - libthrift0_0.8.0_amd64.deb
-	# - libthrift-cil_0.8.0_all.deb
-	# - libthrift-dev_0.8.0_amd64.deb
-	# - libthrift-java_0.8.0_all.deb
-	# - libthrift-perl_0.8.0_all.deb
-	# - libthrift-ruby_0.8.0_amd64.deb
-	# - php5-thrift_0.8.0_amd64.deb
-	# - python-thrift_0.8.0_amd64.deb
-	# - python-thrift-dbg_0.8.0_amd64.deb
-	# - thrift-compiler_0.8.0_amd64.deb
+	# - libthrift0_${thrift_version}_${DEB_BUILD_ARCH}.deb
+	# - libthrift-cil_${thrift_version}_all.deb
+	# - libthrift-dev_${thrift_version}_${DEB_BUILD_ARCH}.deb
+	# - libthrift-java_${thrift_version}_all.deb
+	# - libthrift-perl_${thrift_version}_all.deb
+	# - libthrift-ruby_${thrift_version}_${DEB_BUILD_ARCH}.deb
+	# - php5-thrift_${thrift_version}_${DEB_BUILD_ARCH}.deb
+	# - python-thrift_${thrift_version}_${DEB_BUILD_ARCH}.deb
+	# - python-thrift-dbg_${thrift_version}_${DEB_BUILD_ARCH}.deb
+	# - thrift-compiler_${thrift_version}_${DEB_BUILD_ARCH}.deb
 
 	cd ${repo_root}
 	echo "Built these .deb files:"
 	ls ./*.deb
 	mkdir -p deb
-	mv ${repo_root}/*{.deb,dsc,changes,${THRIFT_VERSION}.tar.gz} ${repo_root}/deb/
+	mv ${repo_root}/*{.deb,dsc,changes,${thrift_version}.tar.gz} ${repo_root}/deb/
 }
 
 function build_fb303
@@ -48,19 +60,19 @@ function build_fb303
 	# We need to install libthrift0, libthrift-dev, thrift-compiler, 
 	# and python-thrift in order to compile fb303.  Go ahead and install
 	# these from the newly created debs.
-	echo "Installing newly created debs for libthrift0, libthrift-dev, thrift-compiler and python-thrift in order to install create fb303 packages."
+	echo "Installing newly created debs for libthrift0, libthrift-dev, thrift-compiler, libthrift-java and python-thrift in order to install create fb303 packages."
 	# uninstall these packages first
-	dpkg -r libthrift0 thrift-compiler libthrift-dev python-thrift
-	dpkg -i ${repo_root}/deb/{libthrift0_0.8.0_amd64.deb,thrift-compiler_0.8.0_amd64.deb,libthrift-dev_0.8.0_amd64.deb,python-thrift_0.8.0_amd64.deb} || (echo "Could not install dependencies for fb303 packages." && exit 1)
+	dpkg -r libthrift0 thrift-compiler libthrift-dev python-thrift libthrift-java
+	dpkg -i ${repo_root}/deb/{libthrift0_${thrift_version}*,thrift-compiler_${thrift_version}*,libthrift-dev_${thrift_version},python-thrift_${thrift_version},libthrift-java_${thrift_version}}*.deb || (echo "Could not install dependencies for fb303 packages." && exit 1)
 
 	echo "Building fb303 packages..."
-	cd ${repo_root}/thrift-$THRIFT_VERSION/contrib/fb303
+	cd ${thrift_directory}/contrib/fb303
 	dpkg-buildpackage
 
 	# This should builds:
-	# - python-fb303_0.8.0_amd64.deb
-	# - libfb303-java_0.8.0_amd64.deb
-	# - thrift-fb303_0.8.0_amd64.deb
+	# - python-fb303_${thrift_version}_${DEB_BUILD_ARCH}.deb
+	# - libfb303-java_${thrift_version}_${DEB_BUILD_ARCH}.deb
+	# - thrift-fb303_${thrift_version}_${DEB_BUILD_ARCH}.deb
 	cd ..
 	echo "Built these .deb files:"
 	ls ./*.deb
@@ -68,13 +80,10 @@ function build_fb303
 	# Now move the created .deb, .changes, .dsc, and source files into deb/
 
 	cd $repo_root
-	mv -v ${repo_root}/thrift-${THRIFT_VERSION}/contrib/*{.deb,dsc,changes,${THRIFT_VERSION}.tar.gz} ${repo_root}/deb/
+	mv -v ${thrift_directory}/contrib/*{.deb,dsc,changes,${thrift_version}.tar.gz} ${repo_root}/deb/
 }
 
-packages=$1
-if [ -z $packages ]; then
-	packages="all"
-fi
+
 
 case $packages in
 	thrift )
